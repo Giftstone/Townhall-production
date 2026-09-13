31919d21acc79292a53712a1e9ca5e3e6b5d1f5c
Gift Stone <giftstone09@gmail.com>
2026-09-13 13:01:20 +0200

    externalize socket.io-client for build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,3 @@
-// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -10,7 +9,7 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
-      external: [],
+      external: ['socket.io-client'],
     }
   }
 });
